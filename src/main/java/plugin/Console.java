db86caa968b7a953b0b85fecd2f9479101497474
CandyLedge <2648851084@qq.com>
2025-06-13 01:32:46 +0800

    refactor(output): 重构控制台输出插件
    - 重新设计了 Console 类的结构，增加了 init 和 consume 方法
    - 使用 Channel<Row> 作为输入，提高了灵活性和可扩展性
    -简化了数据输出逻辑，通过订阅通道实现

--- src/main/java/plugin/Console.java
+++ src/main/java/plugin/Console.java
@@ -1,21 +1,26 @@
 package plugin;
 
 import anno.Output;
-import core.flowdata.RowSetTable;
+import core.Channel;
+import core.flowdata.Row;
 import core.intf.IOutput;
+
 import java.util.Map;
 
 @Output(type = "console")
 public class Console implements IOutput {
+    private Map<String, Object> config;
+
     @Override
-    public void deal(Object input) {
-//        System.out.println("控制台输出插件接收到数据：");
-        if (input instanceof Map) {
-            RowSetTable rst = (RowSetTable) ((Map<?, ?>) input).get("_input");
-            System.out.println(rst.toString());
-        } else {
-            System.err.println("无效的输入类型");
-        }
+    public void init(Map<String, Object> cfg) {
+        this.config = cfg;
+    }
+
+    @Override
+    public void consume(Channel<Row> input) throws Exception {
+        // 订阅通道数据并输出
+        input.subscribe(row -> {
+            System.out.println(row);
+        });
     }
 }
-
