0c165c9b6b548e5bc24e5c7838893b753f8db1b6
CandyLedge <2648851084@qq.com>
2025-06-13 08:56:09 +0800

    refactor: 移除 EtlApplication 中的静态代码块
    移除了 EtlApplication 类中的静态代码块，该代码块调用了 Checker.run("plugin", "anno")。这个修改可能会影响插件和注解相关的功能，需要在其他地方重新实现或替换这部分逻辑。

--- src/main/java/org/gugu/etl/EtlApplication.java
+++ src/main/java/org/gugu/etl/EtlApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EtlApplication {
-    static {
-        Checker.run("plugin", "anno");
-    }
     public static void main(String[] args) {
         SpringApplication.run(EtlApplication.class, args);
     }
