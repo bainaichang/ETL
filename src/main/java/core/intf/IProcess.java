f718c311f3ad1cda7e829ac39e8147ba922b85d8
CandyLedge <2648851084@qq.com>
2025-06-15 20:27:37 +0800

    feat(core): 优化数据传输和处理接口
    - 在 IChannel接口中添加 getHeader 和 setHeader 方法，用于处理报文头信息
    - 修改 IInput、IOutput 和 IProcess 接口，移除冗余的类型参数
    - 更新 IProcess 接口的 process 方法，支持多通道输出

--- src/main/java/core/intf/IProcess.java
+++ src/main/java/core/intf/IProcess.java
@@ -3,9 +3,10 @@ package core.intf;
 import core.Channel;
 import core.flowdata.Row;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IProcess {
     void init(Map<String,Object> cfg);
-    void process(Channel<Row> input,Channel<Row> output) throws Exception;
+    void process(Channel input, List<Channel> outputs) throws Exception;
 }
